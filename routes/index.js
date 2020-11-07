f40efd6cd9389ec570c8270628aa9508493ec573
iamy8000 <amy8000@gmail.com>
2020-11-08 00:33:02 +0800

    feat: adduser module

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const home = require("./modules/home");
-router.use("/", home);
-
 const restaurants = require("./modules/restaurants");
+const users = require('./modules/users')
+
 router.use("/restaurants", restaurants);
+router.use("/", home);
+router.use('/users', users)
 
 module.exports = router;
