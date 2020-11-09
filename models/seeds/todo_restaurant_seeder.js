6d1e2eaadda70628d465c69d3946ca2359aa5315
iamy8000 <amy8000@gmail.com>
2020-11-09 14:53:12 +0800

    modify todo seeder

--- models/seeds/todo_restaurant_seeder.js
+++ models/seeds/todo_restaurant_seeder.js
@@ -1,22 +1,55 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const mongoose = require('mongoose')
 const restaurantList = require('../../restaurant.json').results
-const TodoRestaurant = require('../todo_restaurant') //載入TodoRestaurant models
+const TodoRestaurant = require('../todo_restaurant')
+const User = require('../user')
 const db = require('../../config/mongoose')
+const SEED_USER = [
+  {
+    email: 'user1@example.com',
+    password: '12345678'
+  },
+  {
+    email: 'user2@example.com',
+    password: '12345678'
+  }
+]
 
 db.once('open', () => {
-  for (let i = 0; i < restaurantList.length; i++) {
-    TodoRestaurant.create({
-      id: restaurantList[i].id,
-      name: restaurantList[i].name,
-      name_en: restaurantList[i].name_en,
-      category: restaurantList[i].category,
-      image: restaurantList[i].image,
-      location: restaurantList[i].location,
-      phone: restaurantList[i].phone,
-      google_map: restaurantList[i].google_map,
-      rating: restaurantList[i].rating,
-      description: restaurantList[i].description
-    })
-    console.log('done')
-  }
+  let restaurants = restaurantList
+  SEED_USER.forEach(user => {
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(user.password, salt))
+      .then(hash => User.create({
+        name: user.name,
+        email: user.email,
+        password: hash
+      }))
+      .then(user => {
+        const userId = user._id
+        return Promise.all(Array.from({ length: 3 }, (v, i) => {
+          TodoRestaurant.create({
+            id: restaurants[i].id,
+            name: restaurants[i].name,
+            name_en: restaurants[i].name_en,
+            category: restaurants[i].category,
+            image: restaurants[i].image,
+            location: restaurants[i].location,
+            phone: restaurants[i].phone,
+            google_map: restaurants[i].google_map,
+            rating: restaurants[i].rating,
+            description: restaurants[i].description,
+            userId: user._id
+          })
+        }))
+          .then(restaurants.splice(0, 3))
+        // .then(() => console.log(restaurants))
+      })
+  })
+  console.log('done')
+  // process.exit()
 })
